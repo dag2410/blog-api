e14374983092afceaf24bdc055c595c65b3c9a81
Hai Dang <dagger241004abc@gmail.com>
2025-08-14 18:07:58 +0700

    update

--- src/config/auth.js
+++ src/config/auth.js
@@ -3,7 +3,7 @@ const ms = require("ms");
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
-  REFRESH_TOKEN_EXPIRES_IN: ms(process.env.REFRESH_TOKEN_EXPIRES_IN),
+  REFRESH_TOKEN_EXPIRES_IN: ms(process.env.REFRESH_TOKEN_EXPIRES_IN || "30d"),
   TOKEN_TYPE: process.env.TOKEN_TYPE,
   MAIL_SERVICE: process.env.MAIL_SERVICE,
   MAIL_SENDER_FROM: process.env.MAIL_SENDER_FROM,
